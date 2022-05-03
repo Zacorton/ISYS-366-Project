c9eae582a836785674bd3400fcdde1b0f7a21e59
Zacorton <103071146+Zacorton@users.noreply.github.com>
2022-05-03 18:27:23 -0400

    Add files via upload

---
+++ A to Z Prints_Zs final edit/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+// require MySQL Connection
+require ('database/DBController.php');
+
+// require Product Class
+require ('database/Product.php');
+
+// require Cart Class
+require ('database/Cart.php');
+
+
+// DBController object
+$db = new DBController();
+
+// Product object
+$product = new Product($db);
+$product_shuffle = $product->getData();
+
+// Cart object
+$Cart = new Cart($db );
+$arr = array(
+    "user_id" =>1,
+    "item_id" =>2
+);
+$Cart->insertIntoCart($arr);
